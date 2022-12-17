f3a7bf7b20d6472bdda6c7d6de0ef0f774293af5
unknown <ks305410@student.polsl.pl>
2022-12-17 23:15:59 +0100

    Endpoints modification
    added my name and student number prefix to every endpoint

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/people', [PeopleController::class, 'showAll']);
-Route::get('/people/{id}', [PeopleController::class, 'showOne']);
-Route::post('/people', [PeopleController::class, 'create']);
-Route::put('/people/{id}', [PeopleController::class, 'update']);
-Route::delete('/people/{id}', [PeopleController::class, 'delete']);
+Route::get('/szkolka/305410/people', [PeopleController::class, 'showAll']);
+Route::get('/szkolka/305410/people/{id}', [PeopleController::class, 'showOne']);
+Route::post('/szkolka/305410/people', [PeopleController::class, 'create']);
+Route::put('/szkolka/305410/people/{id}', [PeopleController::class, 'update']);
+Route::delete('/szkolka/305410/people/{id}', [PeopleController::class, 'delete']);
